bd9e9839254b886ad2017076b8357b833f4645a9
luci-e <luciemiliano@gmail.com>
2018-07-24 23:33:54 +0200

    ZA WARUDO TOKI WO TIMERS

--- board_server/Inc/FreeRTOSConfig.h
+++ board_server/Inc/FreeRTOSConfig.h
@@ -89,7 +89,7 @@
 /* Ensure stdint is only used by the compiler, and not the assembler. */
 #if defined(__ICCARM__) || defined(__CC_ARM) || defined(__GNUC__)
     #include <stdint.h>
-    #include <main.hpp> 
+    #include "main.h" 
     extern uint32_t SystemCoreClock;
 #endif
 
@@ -112,6 +112,12 @@
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES                    0
 #define configMAX_CO_ROUTINE_PRIORITIES          ( 2 )
+
+/* Software timer definitions. */
+#define configUSE_TIMERS                         1
+#define configTIMER_TASK_PRIORITY                ( 2 )
+#define configTIMER_QUEUE_LENGTH                 10
+#define configTIMER_TASK_STACK_DEPTH             256
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
